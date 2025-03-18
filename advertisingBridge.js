5178a36a8e93a27d5c1664305854c707e216c9ee
Dzmitry <dshestel@gmail.com>
2025-03-18 15:52:28 +0400

    Add files via upload

--- advertisingBridge.js
+++ advertisingBridge.js
@@ -9,15 +9,6 @@ function showRewarded() {
     });
 }
 
-function showRewardedPop(){
-    show_9040716('pop').then(() => {
-        window.focus();
-        _unityInstance.SendMessage("AdvertisingService", "RewardedShown");
-    }).catch(error => {
-        console.error('Error showing ad:', error);
-    });
-}
-
 function init(unityInstance) {
     _unityInstance = unityInstance;
 }
